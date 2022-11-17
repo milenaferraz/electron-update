9840be76a0a574f9645802f4f8513aa53b4449ff
Milena Ferraz <milenaferraz@gmail.com>
2022-11-17 19:14:51 -0300

    New version

--- script.js
+++ script.js
@@ -3,32 +3,37 @@ const notification = document.getElementById("notification");
 const message = document.getElementById("message");
 const restartButton = document.getElementById("restart-button");
 const version = document.getElementById("version");
+const log = require("electron-log");
 
 ipcRenderer.send("app_version");
 
 ipcRenderer.on("app_version", (event, arg) => {
-  console.log("Script", arg);
+  log.info("Ready", arg);
   ipcRenderer.removeAllListeners("app_version");
   version.innerHTML = "Version " + arg.version;
 });
 
 ipcRenderer.on("update_available", () => {
+  log.info("update_available");
   ipcRenderer.removeAllListeners("update_available");
-  message.innerText = "A new update is available. Downloading now...";
+  message.innerHTML = "A new update is available. Downloading now...";
   notification.classList.remove("hidden");
 });
 
 ipcRenderer.on("update_downloaded", () => {
+  log.info("update_downloaded");
   ipcRenderer.removeAllListeners("update_downloaded");
-  message.innerText =
+  message.innerHTML =
     "Update Downloaded. It will be installed on restart. Restart now?";
   restartButton.classList.remove("hidden");
   notification.classList.remove("hidden");
 });
 
 function closeNotification() {
+  log.info("closeNotification");
   notification.classList.add("hidden");
 }
 function restartApp() {
+  log.info("restartApp");
   ipcRenderer.send("restart_app");
 }
