7127368fb50aa46fd8e6d9d995513095dd773d73
Milena Ferraz <milenaferraz@gmail.com>
2022-11-17 10:23:33 -0300

    Configuration electron

---
+++ main.js
@@ -0,0 +1,61 @@
+const { app, BrowserWindow, ipcMain } = require("electron");
+const { autoUpdater } = require("electron-updater");
+require("dotenv").config();
+
+console.log(process.env.TOKEN_GITHUB);
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true,
+      contextIsolation: false,
+    },
+  });
+
+  mainWindow.loadFile("index.html");
+  mainWindow.on("closed", function () {
+    mainWindow = null;
+  });
+  mainWindow.webContents.openDevTools({ mode: "detach" });
+}
+
+app.on("ready", () => {
+  createWindow();
+});
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+app.once("ready-to-show", () => {
+  autoUpdater.checkForUpdatesAndNotify();
+});
+
+ipcMain.on("app_version", (event) => {
+  console.log("Aqui");
+  event.sender.send("app_version", { version: app.getVersion() });
+});
+
+autoUpdater.on("update-available", () => {
+  mainWindow.webContents.send("update_available");
+});
+autoUpdater.on("update-downloaded", () => {
+  mainWindow.webContents.send("update_downloaded");
+});
+
+ipcMain.on("restart_app", () => {
+  autoUpdater.quitAndInstall();
+});
---
+++ script.js
@@ -0,0 +1,34 @@
+const { ipcRenderer } = require("electron");
+const notification = document.getElementById("notification");
+const message = document.getElementById("message");
+const restartButton = document.getElementById("restart-button");
+const version = document.getElementById("version");
+
+ipcRenderer.send("app_version");
+
+ipcRenderer.on("app_version", (event, arg) => {
+  console.log("Script", arg);
+  ipcRenderer.removeAllListeners("app_version");
+  version.innerHTML = "Version " + arg.version;
+});
+
+ipcRenderer.on("update_available", () => {
+  ipcRenderer.removeAllListeners("update_available");
+  message.innerText = "A new update is available. Downloading now...";
+  notification.classList.remove("hidden");
+});
+
+ipcRenderer.on("update_downloaded", () => {
+  ipcRenderer.removeAllListeners("update_downloaded");
+  message.innerText =
+    "Update Downloaded. It will be installed on restart. Restart now?";
+  restartButton.classList.remove("hidden");
+  notification.classList.remove("hidden");
+});
+
+function closeNotification() {
+  notification.classList.add("hidden");
+}
+function restartApp() {
+  ipcRenderer.send("restart_app");
+}
